fa0735de45a9a591f594132a704ba61da5e19872
Max Bueno <max@1215.io>
2022-01-14 12:12:34 -0600

    Update get_avg_gas.js

--- src/get_avg_gas.js
+++ src/get_avg_gas.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 
-txlist = JSON.parse(fs.readFileSync('./batch_0.json'));
+// txlist = JSON.parse(fs.readFileSync('../data/AaveV2FlashLoanReceiptsMerged.json'));
+var gas = [];
+const files = JSON.parse(fs.readdirSync(`../data/FlashLoanReceipts`));
+//console.log(files);
+for (const filename of files) {
+    txlist = fs.readfileSync(JSON.parse(
+            `../data/FlashLoanReceipts/${filename}`));
+    for (const tx of txlist) {
+      gas.append(tx['gasUsed']);
+    }
+  }
+const sum = gas.reduce((a, b) => a + b, 0);
+const avg = (sum / gas.length) || 0;
+console.log(gas);
 
-        var gas = [];
-        for (let i = 0; i < 1000; i++) {
-        gas.push(txlist[i]['gasUsed']);
-        }
-        const sum = gas.reduce((a, b) => a + b, 0);
-        const avg = (sum / gas.length) || 0;
-    console.log(`The average gas used per transaction is: ${avg}.`);
-    
-    
 
