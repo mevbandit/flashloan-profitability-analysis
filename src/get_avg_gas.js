305ac917b70e25cc2708a450399633009bca0b5f
bandit <94731243+mevbandit@users.noreply.github.com>
2022-01-16 14:21:57 -0500

    Update get_avg_gas.js

--- src/get_avg_gas.js
+++ src/get_avg_gas.js
@@ -1,18 +1,37 @@
 const fs = require('fs');
 
-// txlist = JSON.parse(fs.readFileSync('../data/AaveV2FlashLoanReceiptsMerged.json'));
+// parsing loops. 
 var gas = [];
-const files = JSON.parse(fs.readdirSync(`../data/FlashLoanReceipts`));
-//console.log(files);
+var swaps = [];
+var gasperswap = [];
+var numswaps = [];
+const files = (fs.readdirSync(`../data/FlashLoanReceipts`));
 for (const filename of files) {
-    txlist = fs.readfileSync(JSON.parse(
-            `../data/FlashLoanReceipts/${filename}`));
-    for (const tx of txlist) {
-      gas.append(tx['gasUsed']);
+  txlist = JSON.parse(fs.readFileSync(`../data/FlashLoanReceipts/${filename}`));
+  for (const tx of txlist) {
+    gas.push(tx['gasUsed']);
+    let swapcount = 0;
+    for (const log of tx['logs']) {
+      let topic1 = log['topics'][0];
+      if (topic1 == "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef") {
+        swaps.push(topic1);
+        swapcount ++;
+      }
+      if (swapcount != 0) {
+      gasperswap.push((parseInt(tx['gasUsed']))/(swapcount));
+      numswaps.push(swapcount);
+      }
     }
   }
-const sum = gas.reduce((a, b) => a + b, 0);
-const avg = (sum / gas.length) || 0;
-console.log(gas);
+}
+//console.log(gasperswap);
+ //calculations from the resulting arrays after parsing.
+  const sumgas = gas.reduce((a, b) => a + b, 0);
+  const avggas = (sumgas / gas.length) || 0;
+  const avggasperswap = (gasperswap.reduce((a, b) => a + b, 0) / gasperswap.length);
+  const avgnumswaps =  (numswaps.reduce((a, b) => a + b, 0) / numswaps.length);
+  console.log("avg gas used per tx =", avggas,"avg gas used per swap =",avggasperswap, "avg number of swaps per tx =", avgnumswaps);
+
+
 
 
